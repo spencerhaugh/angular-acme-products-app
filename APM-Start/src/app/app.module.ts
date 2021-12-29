0a994d95a8d705f27717d195cbb8e6f395dbfd3c
Spencer Haugh <spencer.haugh@gmail.com>
2021-12-29 17:00:19 -0500

    add star component and nest in product-list, implement Input

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    StarComponent,
     ConvertToSpacesPipe
   ],
   imports: [
