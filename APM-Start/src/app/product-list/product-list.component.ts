e4972497dcb9803c5332aeefe6a34283f358e2ce
Spencer Haugh <spencer.haugh@gmail.com>
2021-12-30 10:53:01 -0500

    add httpClient and subscription for data retrival

--- APM-Start/src/app/product-list/product-list.component.ts
+++ APM-Start/src/app/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
     displayImages: boolean = false;
     imageWidth: number = 50;
     imageMargin: number = 2;
+    errorMsg: string = '';
 
     constructor(private productService: ProductService) {}
 
@@ -34,8 +35,13 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log("ngOnInit");
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: error => this.errorMsg = error
+        });
     }
 
     toggleImage(): void {
