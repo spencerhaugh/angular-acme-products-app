98f682272ec1350457a009c286b45d0c99181d1b
Spencer Haugh <spencer.haugh@gmail.com>
2021-12-30 14:23:46 -0500

    Implement route guard on invaild product ids

--- APM-Start/src/app/utils/routes.ts
+++ APM-Start/src/app/utils/routes.ts
@@ -1,11 +1,15 @@
 import { WelcomeComponent } from "../components/home/welcome.component";
 import { ProductDetailComponent } from "../components/product-detail/product-detail.component";
 import { ProductListComponent } from "../components/product-list/product-list.component";
+import { ProductDetailGuard } from "../guards/product-detail.guard";
 
 
 export const routes: object[] = [
     { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailComponent },
+    { 
+        path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent },
     { path: 'welcome', component: WelcomeComponent },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
